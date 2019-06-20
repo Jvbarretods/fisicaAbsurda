e7ac62d432831608e989125bc8c9642cedb6b3ad
Marcelo Dias <marcelod789@gmail.com>
2019-06-20 11:58:10 -0300

    Organização das pastas
    As pastas do projeto foram organizadas e também foi adicionado os Scripts de escolha aleatória das salas.

---
+++ IEEE_FisicaAbsurda/Assets/Scripts/RandomSelection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSelection : MonoBehaviour
+{
+    static public List<int> ordemSalas = new List<int>(); //Lista que irá armazenar a ordem das Salas
+    private List<int> ordemTiros = new List<int>(); //Lista que irá armazenar a ordem dos Tiros
+
+    public void ListaRandomSemRepeticao(List<int> lista, int numLimite) //Todos os números entre 1 e o "numLimite" serão salvos aleatóriamente na Lista indicada no parâmetro
+    {
+        for (int i = 0; i < numLimite; i++)
+        {
+        gerar:
+            int n = ObterNumeroAleatorio(numLimite+1);
+
+            if (ExisteNumero(lista, n))
+                goto gerar;
+            else
+            {
+                lista.Add(n);
+                Debug.Log("Número gerado: " + n.ToString());
+            }
+        }
+    }
+
+    private bool ExisteNumero(List<int> lista, int numero) //Compara o número com os elementos da lista, caso o número já esteja na lista ele retorna Verdadeiro
+    {
+        for (int i = 0; i < lista.Count; i++)
+        {
+            if (lista[i] == numero)
+                return true;
+        }
+
+        return false;
+    }
+
+    private int ObterNumeroAleatorio(int i)
+    {
+        System.Random randNum = new System.Random();
+        return randNum.Next(1, i); //Escolhe aleatoriamente números de 1 até "i"
+    }
+
+    protected void LimparLista(List<int> lista)//Removerá todos os elementos da Lista indicada no parâmetro 
+    {
+        for(int i = lista.Count; i > 0 ; i--)
+        {
+            lista.RemoveAt(0);
+            Debug.Log("Tamanho: " + lista.Count);
+        }
+    }
+}
---
+++ IEEE_FisicaAbsurda/Assets/Scripts/RoomChange.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class RoomChange : RandomSelection
+{
+    public GameObject porta;
+    public GameObject spawningLobby, spawningSala1, spawningSala2, spawningSala3, spawningSala4; //Recebem o Objeto que marcará a posição onde o player irá surgir no teletrasporte 
+
+    static public int proximaSala = 0;
+    static public int salasConcluidas = 0;
+
+    static private int k = -1;
+
+    public void TrocaDeSala(Collider2D player)//Realiza o teletrasporte para a próxima sala por meio da lista de ordem randômica
+    {
+        if(k == -1)
+        {
+            k += 1;
+        }
+        else if(k < 4)
+        {
+            proximaSala = ordemSalas[k]; //A "ordemSalas" retorna numeros entre 1 e 4, esses números corresponde as 4 salas do castelo que possuem dificuldade.   
+            k += 1;
+        }
+
+        switch (proximaSala)
+        {
+            case 0:
+                player.transform.position = spawningLobby.transform.position;
+                break;
+            case 1:
+                player.transform.position = spawningSala1.transform.position;
+                salasConcluidas += 1;
+                break;
+            case 2:
+                player.transform.position = spawningSala2.transform.position;
+                salasConcluidas += 1;
+                break;
+            case 3:
+                player.transform.position = spawningSala3.transform.position;
+                salasConcluidas += 1;
+                break;
+            case 4:
+                player.transform.position = spawningSala4.transform.position;
+                salasConcluidas += 1;
+                break;
+            default:
+                Debug.Log("Sala Inválida");
+                break;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        { 
+            TrocaDeSala(other);
+        }
+    }
+}
